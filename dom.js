1ab407086a5ab689e929ee69880f96fbf36dc2e1
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-03-22 20:19:20 -0700

    added DOM manipulation code in js

---
+++ dom.js
@@ -0,0 +1,28 @@
+// console.log(document.head);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.all[10]);
+// console.log(document.all);
+// getElementById //
+//console.log(document.getElementById('header-title'));
+//  const headertitle = document.getElementById('header-title');
+//  console.log(headertitle);
+ 
+//  headertitle.innerText = 'good';
+//  headertitle.textContent = 'hello';
+// console.log(headertitle.innerText);
+// console.log(headertitle.textContent);
+
+// getElemenetsByclassname //
+
+// const Items = document.getElementsByClassName('list-group-item');
+// console.log(Items);
+// console.log(Items[1]);
+// Items[0].innerText = 'hello';
+// console.log(Items[0]);
+// Items[0].style.backgroundColor = 'red';
+// Items[1].style.fontFamily = 'italian';
+// for(i=0; i<Items.length; i++){
+//     Items[i].style.backgroundColor = 'lightgrey';
+}
+
