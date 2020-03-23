0ee69c78061307c4a4faffffbd5b4200204c73df
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-03-22 21:03:16 -0700

    added queryselector Dom manipulation

--- dom.js
+++ dom.js
@@ -24,5 +24,35 @@
 // Items[1].style.fontFamily = 'italian';
 // for(i=0; i<Items.length; i++){
 //     Items[i].style.backgroundColor = 'lightgrey';
-}
 
+
+//Queryselector //
+
+// const header = document.querySelector('#header-title');
+// header.style.borderBottom = ' solid  5px lightgrey';
+// const input = document.querySelector('input');
+// input.style.color = 'blue';
+// input.value ='hello baby';
+// const submit = document.querySelector('input[type= "submit"]');
+// submit.value = 'send';
+// const item = document.querySelector('.list-group-item');
+// item.style.color = 'green';
+// const lastitem = document.querySelector('.list-group-item:last-child')
+// lastitem.style.color = 'violet';
+// const secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.color = 'purple';
+
+
+
+
+// querySelectorAll //
+
+ const titles = document.querySelectorAll('.title');
+ console.log(titles);
+ titles[1].innerText = 'hello babe';
+ const odd = document.querySelectorAll('.list-group-item:nth-child(odd)');
+ const even = document.querySelectorAll('.list-group-item:nth-child(even)');
+ for(i=0; i< odd.length; i++){
+     odd[i].style.backgroundColor = '#ccc';
+     even[i].style.backgroundColor = '#f4f4f4';
+ }
